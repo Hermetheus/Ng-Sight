6c93d0c051437b07d862475fafe119428b1b5807
Hermetheus <allanmolson@gmail.com>
2020-04-21 20:29:58 -0500

    setup Routes

---
+++ src/routes.ts
@@ -0,0 +1,24 @@
+import { SectionHealthComponent } from './app/sections/section-health/section-health.component';
+import { SectionOrdersComponent } from './app/sections/section-orders/section-orders.component';
+import { SectionSalesComponent } from './app/sections/section-sales/section-sales.component';
+import { Routes } from '@angular/router';
+
+export const appRoutes: Routes = [
+  {
+    path: 'sales',
+    component: SectionSalesComponent,
+  },
+  {
+    path: 'orders',
+    component: SectionOrdersComponent,
+  },
+  {
+    path: 'health',
+    component: SectionHealthComponent,
+  },
+  {
+    path: '',
+    redirectTo: '/sales',
+    pathMatch: 'full',
+  },
+];
