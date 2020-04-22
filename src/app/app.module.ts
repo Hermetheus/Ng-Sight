f0b303262a685cc5d9811660ad3518de42d12ee7
Hermetheus <allanmolson@gmail.com>
2020-04-21 20:43:13 -0500

    Added Chart Components & CSS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,9 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 import { SectionSalesComponent } from './sections/section-sales/section-sales.component';
 import { SectionOrdersComponent } from './sections/section-orders/section-orders.component';
 import { SectionHealthComponent } from './sections/section-health/section-health.component';
+import { BarChartComponent } from './charts/bar-chart/bar-chart.component';
+import { LineChartComponent } from './charts/line-chart/line-chart.component';
+import { PieChartComponent } from './charts/pie-chart/pie-chart.component';
 
 @NgModule({
   declarations: [
@@ -20,6 +23,9 @@ import { SectionHealthComponent } from './sections/section-health/section-health
     SectionSalesComponent,
     SectionOrdersComponent,
     SectionHealthComponent,
+    BarChartComponent,
+    LineChartComponent,
+    PieChartComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(appRoutes)],
   providers: [],
