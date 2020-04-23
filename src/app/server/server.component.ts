a3b0d31f0d5d7a9b4f77d6858afba40c8990ed9f
Hermetheus <allanmolson@gmail.com>
2020-04-23 18:52:42 -0500

    Toggle Service Method

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -9,7 +9,29 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ServerComponent implements OnInit {
   constructor() {}
 
+  color: string;
+  buttonText: string;
+
   @Input() serverInput: Server;
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.setServerStatus(this.serverInput.isOnline);
+  }
+
+  setServerStatus(isOnline: boolean) {
+    if (isOnline) {
+      this.serverInput.isOnline = true;
+      this.buttonText = 'Shut Down';
+      this.color = '#66BB6A';
+    } else {
+      this.serverInput.isOnline = false;
+      this.color = '#FF6B6B';
+      this.buttonText = 'Start';
+    }
+  }
+
+  toggleStatus(onlineStatus: boolean) {
+    console.log(this.serverInput.name, ': ', onlineStatus);
+    this.setServerStatus(!onlineStatus);
+  }
 }
